517a720bbc2ad5ad996ef5522bc45e871b639bee
rockermoney <35295528+rockermoney@users.noreply.github.com>
2018-02-02 16:38:57 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int a,b,c,sum,cube,n;
+sum=a*100+b*10+c*1;
+cube=a*a*a+b*b*b+c*c*c;printf("enter the number is:");
+scanf("%d",&n);
+if(sum==cube)
+printf("yes");
+else
+printf("no");
+return 0;
+}
