14652d6b7bd4b5bb38f88dc7fae6553374305cf6
rockermoney <35295528+rockermoney@users.noreply.github.com>
2018-02-02 16:21:44 +0530

    Update odd.c

--- odd.c
+++ odd.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int i,n,m;
-      printf("enter the numbers:");
-    scanf("%d%d",&m,&n);
-  printf("All odd numbers from %d to %d are:\n",m,n);
-   for(i=m;i<n;i++)
-    {
-              if(i%2!=0)
-        {
-            printf("%d\n",i);
-        }
-        
-    }
-   return i;
+int i,n,m;
+printf("enter the numbers:");
+scanf("%d%d",&m,&n);
+printf("All odd numbers from %d to %d are:\n",m,n);
+for(i=m;i<n;i++)
+ if(i%2!=0)
+ {
+ printf("%d\n",i);
+ }
+ }
+ return i;
 }
